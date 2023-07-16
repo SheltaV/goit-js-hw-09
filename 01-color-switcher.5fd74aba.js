d91a531ec0219f376bce1759ca4fd4023a1bf4b8
SheltaV <124499370+SheltaV@users.noreply.github.com>
2023-07-16 12:56:00 +0000

    Deploying to gh-pages from @ SheltaV/goit-js-hw-09@6e60335eb0f8368064eeaaae2b6728c4fcf51862 🚀

---
+++ 01-color-switcher.5fd74aba.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(a)}));var a=null}();
+//# sourceMappingURL=01-color-switcher.5fd74aba.js.map
